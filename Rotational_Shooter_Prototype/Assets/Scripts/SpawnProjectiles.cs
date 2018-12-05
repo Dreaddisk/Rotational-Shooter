185157745ef167a81d8a90789a0777b909e42308
Jose M. Puebla <joey_manuel@outlook.com>
2018-12-05 13:11:02 -0500

    2. movement and shooting
    - Implemented already the Player rotation and the shooting projectile.

---
+++ Rotational_Shooter_Prototype/Assets/Scripts/SpawnProjectiles.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProjectiles : MonoBehaviour
+{
+    #region Variables
+    public GameObject projectile;
+    #endregion
+
+    private void Update()
+    {
+        GetMouseClickDown();
+    }
+
+    void GetMouseClickDown()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            SpawnTheProjectileVOID();
+        }
+    }
+
+    void SpawnTheProjectileVOID()
+    {
+        GameObject projectileClone;
+
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       projectileClone = Instantiate(projectile, transform.position, Quaternion.Euler(0, 0, transform.eulerAngles.z)) as GameObject;
+
+        Rigidbody projectileRigidBody = projectileClone.AddComponent<Rigidbody>();
+        projectileRigidBody.velocity = mousePosition * 4.0f;
+        projectileRigidBody.useGravity = false;
+    }
+} // main class
