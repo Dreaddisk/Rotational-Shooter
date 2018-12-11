4cd15da861b7ad62e414cf1435cdbb7e8d343cc3
Jose M. Puebla <joey_manuel@outlook.com>
2018-12-10 21:31:51 -0500

    3. Enemy Logic

---
+++ Rotational_Shooter_Prototype/Assets/Scripts/EnemyMovementLogic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementLogic : MonoBehaviour
+{
+    #region Variables
+
+    #endregion
+
+    private void Update()
+    {
+        MoveEnemyToPlayer();
+    }
+
+    void MoveEnemyToPlayer()
+    {
+        transform.position = Vector3.Slerp(transform.position, new Vector3(0, 0, 0), 1.0f * Time.deltaTime);
+    }
+
+} // main class
---
+++ Rotational_Shooter_Prototype/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    #region Variables
+    public float enemySpawnTimeFloat = 1.0f;
+    public GameObject enemyToSpawn;
+    public bool canSpawn = true;
+    #endregion
+
+    private void Start()
+    {
+        StartCoroutine(EnemySpawnTimer());
+    }
+
+    IEnumerator EnemySpawnTimer()
+    {
+        while(canSpawn == true)
+        {
+            SpawnEnemy0();
+        }
+
+        yield return new WaitForSeconds(enemySpawnTimeFloat);
+    }
+
+    void SpawnEnemy0()
+    {
+        Instantiate(enemyToSpawn, new Vector3(-38f, 18f, 0f), Quaternion.identity);
+    }
+} // main class
